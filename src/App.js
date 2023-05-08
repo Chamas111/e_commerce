c19ca4640a9eee3d6669c68d59b959ef3cc35fad
Chamas111 <ahmad.chamas87@gmail.com>
2023-05-08 15:24:22 +0200

    create index file in the Component folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Products from "./components/Products/Products";
-import Navbar from "./components/Navbar/Navbar";
-
+import { Navbar, Products } from "./components";
 function App() {
   return (
     <div>
